d9a2c22a57888382deb977c8dabe6bee9cdbb1cd
Artur Sum <sum.artur@gmail.com>
2016-11-05 18:06:58 +0100

    karma & jasmine installed

---
+++ Test/Tetris.spec.js
@@ -0,0 +1,12 @@
+describe('Tetris Class', ()=>{
+
+    var tetris = new Tetris();
+
+
+    it('should have method set', ()=>{
+        expect(typeof tetris.set).toBe('function');
+    });
+
+
+
+});
